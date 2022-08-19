7d459491461cc763ff586c9a258c8f32edc65809
Benonardo <benonardo@icloud.com>
2022-08-19 18:19:44 +0200

    Make beacon translucent

--- src/main/java/com/benonardo/translucentglass/mixin/RenderLayersMixin.java
+++ src/main/java/com/benonardo/translucentglass/mixin/RenderLayersMixin.java
@@ -11,13 +11,18 @@ import org.spongepowered.asm.mixin.injection.Slice;
 @Mixin(RenderLayers.class)
 public class RenderLayersMixin {
 
-    @ModifyArg(method = "method_23685", at = @At(value = "INVOKE", target = "Ljava/util/HashMap;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", remap = false, ordinal = 0), slice = @Slice(from = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;GLASS:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC)), index = 1)
+    @ModifyArg(method = "method_23685", remap = false, at = @At(value = "INVOKE", target = "Ljava/util/HashMap;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", remap = false, ordinal = 0), slice = @Slice(from = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;GLASS:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC)), index = 1)
     private static Object makeGlassTranslucent(Object original) {
         return RenderLayer.getTranslucent();
     }
 
-    @ModifyArg(method = "method_23685", at = @At(value = "INVOKE", target = "Ljava/util/HashMap;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", remap = false, ordinal = 0), slice = @Slice(from = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;GLASS_PANE:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC)), index = 1)
+    @ModifyArg(method = "method_23685", remap = false, at = @At(value = "INVOKE", target = "Ljava/util/HashMap;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", remap = false, ordinal = 0), slice = @Slice(from = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;GLASS_PANE:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC)), index = 1)
     private static Object makeGlassPaneTranslucent(Object original) {
+        return RenderLayer.getTranslucent();
+    }
+
+    @ModifyArg(method = "method_23685", remap = false, at = @At(value = "INVOKE", target = "Ljava/util/HashMap;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", remap = false, ordinal = 0), slice = @Slice(from = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;BEACON:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC)), index = 1)
+    private static Object makeBeaconTranslucent(Object original) {
         return RenderLayer.getTranslucent();
     }
 
